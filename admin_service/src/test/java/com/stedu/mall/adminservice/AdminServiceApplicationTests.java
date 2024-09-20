ecd17636d2d86b1e281e324a0f30917ead3b11b6
AsKiro <1720194603@qq.com>
2024-09-20 11:59:48 +0800

    "初始化本地仓库，首次提交"

---
+++ admin_service/src/test/java/com/stedu/mall/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stedu.mall.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
