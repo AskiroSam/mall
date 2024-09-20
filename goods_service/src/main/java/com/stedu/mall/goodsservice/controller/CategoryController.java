e51e56d24a1d3faed5d1524045e3b4373b35a518
AsKiro <1720194603@qq.com>
2024-09-20 19:55:42 +0800

    "分类管理controller层开发"

---
+++ goods_service/src/main/java/com/stedu/mall/goodsservice/controller/CategoryController.java
@@ -0,0 +1,62 @@
+package com.stedu.mall.goodsservice.controller;
+
+import com.stedu.mall.common.bean.Category;
+import com.stedu.mall.common.bean.RespBean;
+import com.stedu.mall.common.exception.SteduException;
+import com.stedu.mall.common.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/Category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    //添加
+    @PostMapping
+    public RespBean insert(@RequestBody Category category) throws SteduException {
+        categoryService.insert(category);
+        return RespBean.ok("添加成功");
+    }
+
+    //根据id删除
+    @DeleteMapping("/{id}")
+    public RespBean delete(@PathVariable("id") Integer id) throws SteduException {
+        categoryService.delete(id);
+        return RespBean.ok("删除成功");
+    }
+
+    //根据id修改
+    @PutMapping
+    public RespBean update(@RequestBody Category category) throws SteduException {
+        categoryService.update(category);
+        return RespBean.ok("修改成功");
+    }
+
+    //分页搜索
+    @GetMapping("/search")
+    public RespBean selectByPage(Category category, Integer pageNum, Integer pageSize) {
+       pageNum = pageNum == null ? 1 : pageNum;
+       pageSize = pageSize == null ? 5 : pageSize;
+
+       Object data = categoryService.selectByCondition(category, pageNum, pageSize);
+       return RespBean.ok("", data);
+    }
+
+    //根据id查询
+    @GetMapping("/{id}")
+    public RespBean selectById(@PathVariable("id") Integer id) {
+        return RespBean.ok("", categoryService.selectBtyId(id));
+    }
+
+    //获取所有的夫分类
+    @GetMapping("/allParent")
+    public RespBean selectAllParent() {
+        Category condition = new Category();
+        condition.setParentId(0);
+
+        Object data = categoryService.selectByCondition(condition, null, null);
+        return RespBean.ok("", data);
+    }
+}
