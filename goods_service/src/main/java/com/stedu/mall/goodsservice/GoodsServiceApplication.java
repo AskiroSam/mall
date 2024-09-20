abec1682d980cb268a85ecff54e7f6e8f68aad76
AsKiro <1720194603@qq.com>
2024-09-20 21:21:28 +0800

    "前端布局"

--- goods_service/src/main/java/com/stedu/mall/goodsservice/GoodsServiceApplication.java
+++ goods_service/src/main/java/com/stedu/mall/goodsservice/GoodsServiceApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//scanBasePackages = 让springBoot扫描特定的包
 @SpringBootApplication(scanBasePackages = "com.stedu.mall")
 @MapperScan(basePackages = "com.stedu.mall.goodsservice.mapper")
 public class GoodsServiceApplication {
