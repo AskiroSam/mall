464346b04a3ede9688e6f91d1ff5e90c28b1158d
AsKiro <1720194603@qq.com>
2024-09-24 15:17:44 +0800

    完成商品管理前端接口的开发

--- front/src/api/goodsApi.js
+++ front/src/api/goodsApi.js
@@ -3,31 +3,27 @@ import service from "@/api/index.js";
 const goodsApi = {
     //添加
     insert(goods) {
-        return service.post('/category', goods);
+        return service.post('/goods', goods);
     },
     //删除
     delete(id) {
-        return service.delete(`/category/${id}`);
+        return service.delete(`/goods/${id}`);
     },
     //修改
-    update(category) {
-        return service.put('/category', category);
+    update(goods) {
+        return service.put('/goods', goods);
+    },
+    //根据id查询
+    selectById(id) {
+    return service.get(`/goods/${id}`);
     },
     //分页搜索
     selectByPage(condition, pageNum, pageSize) {
         condition.pageNum = pageNum;
         condition.pageSize = pageSize;
-        return service.get('/category/search', {
+        return service.get('/goods', {
             params: condition
-        })
-    },
-    //根据id搜索
-    selectById(id) {
-        return service.get(`/category/${id}`);
-    },
-    //查询所有的夫分类
-    selectAllParent() {
-        return service.get('/category/allParent');
+        });
     }
 }
 export default goodsApi;
