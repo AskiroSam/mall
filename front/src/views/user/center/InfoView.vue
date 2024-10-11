7bfcbf22987a8e3ceb1b3ab2b5504501ad74b6ab
AsKiro <1720194603@qq.com>
2024-10-11 22:26:22 +0800

    完成个人中心的我的订单和个人信息（初版）

--- front/src/views/user/center/InfoView.vue
+++ front/src/views/user/center/InfoView.vue
@@ -1,11 +1,109 @@
 <template>
-个人信息
+  <el-card>
+    <div>
+      <div class="header">
+        <div class="left">
+          <el-image src="/src/images/tx.jpg" class="leftImg" />
+        </div>
+        <div class="main">
+          <el-row>
+            <el-col class="main_userName">{{user.username}}</el-col>
+            <el-col class="main_text">欢迎光临~</el-col>
+            <el-col class="main_router">修改个人信息></el-col>
+          </el-row>
+        </div>
+        <div class="right">
+          <el-row>
+            <el-col class="right_text">
+              账户状态：<span style="color:var(--theme-color);">{{user.status === 0 ? '未认证' : user.status === 1 ? '已认证' : user.status === 2 ? '禁用' : '注销'}}</span>
+            </el-col>
+            <el-col class="right_text">
+              绑定手机：<span>{{user.phone}}</span>
+            </el-col>
+            <el-col class="right_text">
+              绑定邮箱：<span>{{user.email}}</span>
+            </el-col>
+          </el-row>
+        </div>
+      </div>
+      <div class="main"></div>
+    </div>
+  </el-card>
 </template>
 
 <script setup>
+import userApi from "@/api/userApi.js";
+import {ref} from "vue";
 
+//保存用户信息
+const user = ref({
+  username: '',
+  status: null,
+  phone: '',
+  email: ''
+});
+
+//获取用户信息
+function getInfo() {
+  userApi.getInfo()
+      .then(resp => {
+        user.value = resp.data;
+      })
+}
+
+getInfo();
 </script>
 
 <style scoped>
+.header {
+  display: flex;
+}
 
+.left {
+  margin-top: 20px;
+  margin-left: 50px;
+  float: left;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.leftImg {
+  width: 160px;
+  height: 160px;
+  border: 3px double #8c939d;
+  border-radius: 50%;
+}
+
+.main_userName {
+  margin-top: 50px;
+  margin-left: 20px;
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.main_text {
+  margin-top: 5px;
+  margin-left: 20px;
+  font-size: 15px;
+  color: #999999;
+}
+
+.main_router {
+  margin-top: 15px;
+  margin-left: 20px;
+  font-size: 12px;
+  color: var(--theme-color);
+}
+
+.right {
+  float: right;
+  margin-top: 25px;
+}
+
+.right_text {
+  margin-top: 20px;
+  margin-left: 80px;
+  font-size: 14px;
+  color: #999999;
+}
 </style>
--- front/src/views/user/center/OrderListView.vue
+++ front/src/views/user/center/OrderListView.vue
@@ -1,11 +1,163 @@
 <template>
-我的订单
+  <el-card>
+    <div>
+      <div class="header">
+        <div class="top">
+          <span class="orderText">我的订单</span>
+          <span class="promptText">请谨防钓鱼链接或诈骗电话</span>
+        </div>
+        <div class="main">
+          <el-row v-for="(order, index) in orderList" :key="index" class="orderItem">
+            <el-card style="width: 900px; background-color: #EEEEEE;">
+              <el-row style="margin-top: 0">
+                <el-col :span="3" style="font-size: 12px; margin-left: 20px; margin-top: 20px">{{formatDate(order.createTime)}}</el-col>
+                <el-col :span="6" style="font-size: 12px; margin-left: 20px; margin-top: 20px">订单号：{{order.id}}</el-col>
+              </el-row>
+              <el-row style="background-color: #FFFFFF; margin-bottom: 0" v-for="(orderDetail, index) in order.orderDetailList" :key="index">
+                <el-col :span="3">
+                  <div class="pic">
+                    <el-image
+                        :src="`${SERVER_ADDR}/goods/pic/${orderDetail.goods.picList[0].url}`"
+                        style="width: 100px; height: 100px; margin-top: 10px; margin-left: 10px"
+                        fit="contain" />
+                  </div>
+                </el-col>
+                <el-col :span="4" style="margin-top: 60px; margin-left: 10px">{{ orderDetail.goods.name}}</el-col>
+                <el-col :span="3" style="margin-top: 30px; margin-left: 10px"><div class="price">￥{{ orderDetail.goods.markPrice}}</div>
+                  ￥{{ orderDetail.goods.price}}
+                </el-col>
+                <el-col :span="3" style="margin-top: 60px; margin-left: 10px">{{orderDetail.count}}</el-col>
+                <el-col :span="3" style="margin-top: 60px; margin-left: 10px">￥{{ orderDetail.price}}
+                  <div class="carriage">(含运费:￥0.00)</div>
+                </el-col>
+                <el-col :span="4" style="margin-top: 60px; margin-left: 80px">{{getOrderStatus(order.status)}}</el-col>
+              </el-row>
+            </el-card>
+          </el-row>
+        </div>
+      </div>
+    </div>
+  </el-card>
 </template>
 
 <script setup>
+import orderApi from "@/api/orderApi.js";
+import {ref} from "vue";
+import userApi from "@/api/userApi.js";
+import {Delete} from "@element-plus/icons-vue";
 
+//服务器地址
+const SERVER_ADDR = ref(import.meta.env.VITE_SERVER_ADDR);
+//存放该用户的所有订单
+const orderList = ref([]);
+//存放该用户订单中的订单详情
+const orderDetailList = ref([]);
+//保存用户信息
+const user = ref({
+  userId: null
+});
+
+//获取当前用户的订单信息
+function getOrderList() {
+  const condition = {
+    userId: user.value.id
+  }
+  orderApi.selectByPage(condition)
+      .then(resp => {
+        orderList.value = resp.data.list;
+        console.log(orderList.value)
+        //将订单详情保存
+        resp.data.list.forEach(o => {
+          o.orderDetailList.forEach(d => {
+            orderDetailList.value.push(d);
+          })
+        })
+        console.log(orderDetailList.value);
+
+      })
+}
+
+//获取用户信息
+function getInfo() {
+  userApi.getInfo()
+      .then(resp => {
+        user.value = resp.data;
+        getOrderList();
+      })
+}
+
+//设置显示的订单时间格式
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+}
+
+//通过status来决定订单状态
+function getOrderStatus(status) {
+  const statusMap = {
+    0: '未支付',
+    1: '已支付',
+    2: '已发货',
+    3: '已收货',
+    4: '退货退款',
+    5: '退换货',
+    6: '仅退款',
+    7: '售后',
+    8: '其它'
+  };
+  return statusMap[status]; // 默认处理未知状态
+}
+
+getInfo();
 </script>
 
 <style scoped>
+.el-card {
+  --el-card-padding: 0;
+}
 
+.header {
+  margin-top: 30px;
+  margin-left: 30px;
+}
+
+.orderText {
+  margin-top: 50%;
+  color: #8c939d;
+  font-size: 35px;
+}
+
+.promptText {
+  margin-left: 10px;
+  color: #999999;
+  font-size: 13px;
+}
+
+.main {
+  margin-top: 20px;
+}
+
+.main ul li {
+  margin-top: 30px;
+  float: left;
+  margin-left: 0;
+  margin-right: 40px;
+}
+
+.main div {
+  margin-top: 2px;
+  margin-bottom: 10px;
+}
+
+.price {
+  margin-left: 5px;
+  font-size: 13px;
+  color: #8c939d;
+  text-decoration: line-through;
+}
+
+.carriage {
+  color: #8c939d;
+  font-size: 15px;
+}
 </style>
